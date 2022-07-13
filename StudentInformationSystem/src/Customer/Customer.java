8470d3324cc67cc8d8144adb763da6a2a53f8622
Bhaskar Kumbhar <71270951+bhaskar2311@users.noreply.github.com>
2022-07-13 13:09:24 +0530

    Update Customer.java

--- StudentInformationSystem/src/Customer/Customer.java
+++ StudentInformationSystem/src/Customer/Customer.java
@@ -3,18 +3,7 @@ package Customer;
 public class Customer {
 	int id;
 	String name;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	
 	
 	
 	
